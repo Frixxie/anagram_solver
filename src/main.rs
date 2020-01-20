ecc91bdf2a0af327205bedb22822737b7a6fc7fd
Frixxie <freddahf@outlook.com>
2020-01-20 21:36:14 +0000

    Made factoring changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
 use regex::Regex;
+use std::char;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::char;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,7 +12,7 @@ fn main() {
     }
 
     let dict = fs::read_to_string(&args[1]).unwrap();
-    let phrase = &args[2]; 
+    let phrase = &args[2];
 
     let primes: Vec<bool> = seive_of_eratosthenes(103);
     /*
@@ -35,7 +35,7 @@ fn main() {
     */
 
     let lookuptable = create_lookuptable(dict.replace("\n", " "), lettermap.to_owned());
-    
+
     /*
     for i in lookuptable.keys().zip(lookuptable.values()) {
         let (key, value) = i;
@@ -44,9 +44,23 @@ fn main() {
     //println!("{}", lookuptable.len());
     */
 
-    if lookuptable.contains_key(&translate_word_to_num(phrase.to_string().to_lowercase(), lettermap.to_owned())) { 
-        let list: Vec<String> = lookuptable.get(&translate_word_to_num(phrase.to_string().to_lowercase(), lettermap.to_owned())).unwrap().to_vec();
-        println!("{}:{}:{:?}",phrase,translate_word_to_num(phrase.to_string().to_lowercase(), lettermap.to_owned()), list);
+    if lookuptable.contains_key(&translate_word_to_num(
+        phrase.to_string().to_lowercase(),
+        lettermap.to_owned(),
+    )) {
+        let list: Vec<String> = lookuptable
+            .get(&translate_word_to_num(
+                phrase.to_string().to_lowercase(),
+                lettermap.to_owned(),
+            ))
+            .unwrap()
+            .to_vec();
+        println!(
+            "{}:{}:{:?}",
+            phrase,
+            translate_word_to_num(phrase.to_string().to_lowercase(), lettermap.to_owned()),
+            list
+        );
     } else {
         println!("No match for {}", phrase)
     }
@@ -56,7 +70,7 @@ fn create_lettermap(primes: Vec<bool>) -> HashMap<char, i8> {
     let mut lettermap: HashMap<char, i8> = HashMap::new();
     let mut j: usize = 2;
     for i in 0..26 {
-        if primes[j] == true { 
+        if primes[j] == true {
             lettermap.insert(char::from_digit((i + 10) as u32, 36).unwrap(), j as i8);
             j += 1
         }
@@ -70,7 +84,7 @@ fn create_lettermap(primes: Vec<bool>) -> HashMap<char, i8> {
 fn create_lookuptable(dict: String, lettermap: HashMap<char, i8>) -> HashMap<i128, Vec<String>> {
     let mut lookuptable: HashMap<i128, Vec<String>> = HashMap::new();
     let re = Regex::new(r"([^a-zA-Z])+").unwrap();
-    
+
     let matches: Vec<&str> = re.split(dict.as_str()).collect();
     for word in &matches {
         let val = translate_word_to_num(word.to_string().to_lowercase(), lettermap.to_owned());
@@ -78,8 +92,7 @@ fn create_lookuptable(dict: String, lettermap: HashMap<char, i8>) -> HashMap<i12
             let mut list: Vec<String> = Vec::new();
             list.push(word.to_string().to_lowercase());
             lookuptable.insert(val, list);
-        }
-        else {
+        } else {
             let mut list: Vec<String> = lookuptable.get(&val).unwrap().to_vec();
             if !list.contains(&word.to_string().to_lowercase()) {
                 list.push(word.to_string().to_lowercase());
@@ -105,9 +118,9 @@ fn seive_of_eratosthenes(n: i32) -> Vec<bool> {
     primes[0] = false;
     primes[1] = false;
     let mut i: usize = 2;
-    while i*i <= n as usize {
+    while i * i <= n as usize {
         if primes[i] == true {
-            let mut j = i*i;
+            let mut j = i * i;
             while j <= n as usize {
                 primes[j] = false;
                 j += i;
